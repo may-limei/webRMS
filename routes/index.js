cbf262caee845fe77305f4fe24b71bb56e8eccfa
may-limei <limei_wu@yeah.net>
2018-01-04 21:32:01 +0800

    change the slide images, the images cannot display  normally

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Radiation Monitor System | SINAP' });
+  res.render('index', { title: '' });
 });
 
 module.exports = router;
