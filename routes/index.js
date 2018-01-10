602d9579f94f1cbf9ec0a69c8857ea78404df398
may-limei <limei_wu@yeah.net>
2018-01-10 10:04:44 +0800

    title处改为中文

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: '主页' });
 });
 
 router.get('/index.html',function(req,res,next){
-  res.render('index',{ title: 'Home' });
+  res.render('index',{ title: '主页' });
 });
 
 router.get('/point-environment.html',function(req,res,next){
-  res.render('point-environment',{ title: 'monitor point - environment' });
+  res.render('point-environment',{ title: '监测点 - 环境' });
 });
 
 router.get('/point-103.html',function(req,res,next){
-  res.render('point-103',{ title: 'monitor point - 103' });
+  res.render('point-103',{ title: '监测点 - 环境103楼' });
 });
 
 router.get('/point-104.html',function(req,res,next){
-  res.render('point-104',{ title: 'monitor point - 104' });
+  res.render('point-104',{ title: '监测点 - 环境104楼' });
 });
 
 router.get('/point-106.html',function(req,res,next){
-  res.render('point-106',{ title: 'monitor point - 106' });
+  res.render('point-106',{ title: '监测点 - 环境106楼' });
 });
 
 router.get('/point-109.html',function(req,res,next){
-  res.render('point-109',{ title: 'monitor point - 109' });
+  res.render('point-109',{ title: '监测点 - 环境109楼' });
 });
 
 router.get('/point-112.html',function(req,res,next){
-  res.render('point-112',{ title: 'monitor point - 112' });
+  res.render('point-112',{ title: '监测点 - 环境112楼' });
 });
 
 router.get('/point-115.html',function(req,res,next){
-  res.render('point-115',{ title: 'monitor point - 115' });
+  res.render('point-115',{ title: '监测点 - 环境115楼' });
 });
 
 router.get('/left-sidebar.html',function(req,res,next){
